2a1baee420a46915d179c08560bcee8a015ed814
João Vitor Lima <jvolima2004@gmail.com>
2022-08-16 10:09:04 -0300

    Default Layout para as rotas

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 4rem 1rem 8.375rem;
+  background: ${(props) => props.theme['base-profile']};
+
+  display: flex;
+  justify-content: center;
+`
